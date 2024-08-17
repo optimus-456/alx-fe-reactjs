fc0bfbf50bfe1405eaac91905f3fc9cec8d89495
Optimus <ijaolatoyosi03@gmail.com>
2024-08-17 18:20:00 +0100

    Adding styles to components

---
+++ alx-react-app-new/src/components/Header.jsx
@@ -0,0 +1,23 @@
+function Header() {
+  return (
+    <div>
+      <header>
+        <h1
+          style={{
+            backgroundColor: "navy",
+            color: "white",
+            textAlign: "center",
+            padding: "10px",
+            borderRadius: "20px",
+            width: "60%",
+            marginLeft: "200px",
+          }}
+        >
+          My Favorite Cities
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
---
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -0,0 +1,52 @@
+const UserProfile = (props) => {
+  return (
+    <div>
+      <h2
+        style={{
+          backgroundColor: "grey",
+          color: "white",
+          textAlign: "center",
+          padding: "10px",
+          borderRadius: "20px",
+          width: "60%",
+          marginLeft: "200px",
+          border: "1px solid black",
+        }}
+      >
+        {props.name}
+      </h2>
+      <p
+        style={{
+          backgroundColor: "grey",
+          color: "white",
+          textAlign: "center",
+          padding: "10px",
+          borderRadius: "20px",
+          width: "60%",
+          marginLeft: "200px",
+          border: "1px solid black",
+          fontSize: "25px",
+        }}
+      >
+        Age: {props.age}
+      </p>
+      <p
+        style={{
+          backgroundColor: "grey",
+          color: "white",
+          textAlign: "center",
+          padding: "10px",
+          borderRadius: "20px",
+          width: "60%",
+          marginLeft: "200px",
+          border: "1px solid black",
+          fontSize: "25px",
+        }}
+      >
+        Bio: {props.bio}
+      </p>
+    </div>
+  );
+};
+
+export default UserProfile;
