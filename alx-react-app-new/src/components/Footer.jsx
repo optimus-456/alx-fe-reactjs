90a32c4e39d43175ec39094efacd407136dceb2d
Optimus <ijaolatoyosi03@gmail.com>
2024-08-17 18:31:34 +0100

    Added inline styling

--- alx-react-app-new/src/components/Footer.jsx
+++ alx-react-app-new/src/components/Footer.jsx
@@ -1,7 +1,18 @@
 function Footer() {
   return (
     <div>
-      <footer>
+      <footer
+        style={{
+          backgroundColor: "grey",
+          color: "blue",
+          textAlign: "center",
+          padding: "10px",
+          borderRadius: "20px",
+          width: "60%",
+          marginLeft: "200px",
+          border: "1px solid black",
+        }}
+      >
         <p>© 2023 City Lovers</p>
       </footer>
     </div>
--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,7 +1,18 @@
 function MainContent() {
   return (
     <div>
-      <main>
+      <main
+        style={{
+          backgroundColor: "grey",
+          color: "blue",
+          textAlign: "center",
+          padding: "10px",
+          borderRadius: "20px",
+          width: "60%",
+          marginLeft: "200px",
+          border: "1px solid black",
+        }}
+      >
         <p>I love to visit New York, Paris, and Tokyo.</p>
       </main>
     </div>
