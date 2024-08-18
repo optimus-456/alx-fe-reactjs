c2ee8c2810abb9cf46e57824c1c2a1f593fb7adb
Optimus <ijaolatoyosi03@gmail.com>
2024-08-18 23:18:10 +0100

    Made some directory changes

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./Home";
-import About from "./About";
-import Services from "./Services";
-import Contact from "./Contact";
-import Navbar from "./Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Services from "./components/Services";
+import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
