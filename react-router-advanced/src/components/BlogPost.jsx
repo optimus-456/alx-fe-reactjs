c48798326f5e3579cbe8e27b07c090eb784442b1
Optimus <ijaolatoyosi03@gmail.com>
2024-09-01 15:53:06 +0100

    Made some requirement changes

--- react-router-advanced/src/components/BlogPost.jsx
+++ react-router-advanced/src/components/BlogPost.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import { useParams } from "react-router-dom";
 
 function BlogPost() {
-  const { postId } = useParams();
+  const { id } = useParams();
   return (
     <div>
       <h3>Blog Post</h3>
-      <p>Displaying blog post with ID: {postId}</p>
+      <p>Displaying blog post with ID: {id}</p>
     </div>
   );
 }
