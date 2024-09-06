b82a4995f1dae5266b25bf1b8a4d614c3aed9a36
Optimus <ijaolatoyosi03@gmail.com>
2024-09-06 23:59:47 +0100

    Responsiveness

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,6 +1,6 @@
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg  sm:p-4 max-w-xs md:p-8 max-w-sm">
+    <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg  sm:p-4 max-w-xs md:max-w-sm p-8">
       <img
         className="rounded-full w-36 h-36 mx-auto sm:w-24 h-24 md:w-36 h-36 "
         src="https://via.placeholder.com/150"
