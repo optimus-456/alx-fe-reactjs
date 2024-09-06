69ac2151f1de54a0504a063a1cdabf2376fc29a8
Optimus <ijaolatoyosi03@gmail.com>
2024-09-07 00:03:58 +0100

    Made some more changes

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -2,7 +2,7 @@ function UserProfile() {
   return (
     <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg  sm:p-4 max-w-xs md:max-w-sm md:p-8">
       <img
-        className="rounded-full w-36 h-36 mx-auto sm:w-24 h-24 md:w-36 h-36 "
+        className="rounded-full w-36 h-36 mx-auto sm:w-24 sm:h-24 md:w-36 md:h-36 "
         src="https://via.placeholder.com/150"
         alt="User"
       />
