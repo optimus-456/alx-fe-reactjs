344895c5972f2fb60b5a7d2e1dfc0f50fe0e9f29
Optimus <ijaolatoyosi03@gmail.com>
2024-09-07 00:11:46 +0100

    Added text effects and hover effects

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,12 +1,12 @@
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg  sm:p-4 max-w-xs md:max-w-sm md:p-8">
+    <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg  sm:p-4 max-w-xs md:max-w-sm md:p-8 hover:shadow-xl">
       <img
-        className="rounded-full w-36 h-36 mx-auto sm:w-24 sm:h-24 md:w-36 md:h-36 "
+        className="rounded-full w-36 h-36 mx-auto sm:w-24 sm:h-24 md:w-36 md:h-36 hover:scale-110 transition-transform duration-300 ease-in-out "
         src="https://via.placeholder.com/150"
         alt="User"
       />
-      <h1 className="text-xl text-blue-800 my-4  sm:text-lg md:text-xl">
+      <h1 className="text-xl text-blue-800 my-4  sm:text-lg md:text-xl hover:text-blue-500 cursor-pointer">
         John Doe
       </h1>
       <p className="text-gray-600 text-base sm:text-sm md:text-base">
