ce2bd84aa52eb4fdaddfcdea5070c7b1ce7beb38
Optimus <ijaolatoyosi03@gmail.com>
2024-09-22 22:27:18 +0100

    Final deployment changes

--- github-user-search/vite.config.js
+++ github-user-search/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      external: [
+        // Add the modules that need to be treated as external dependencies
+        "react",
+        "react-dom",
+        "axios", // Add any other libraries here as needed
+      ],
+    },
+  },
+});
